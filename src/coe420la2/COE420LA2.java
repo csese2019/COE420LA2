c97ad787faa87dfa4783fdb682d3c6d9c5fc9e98
Maryam Mustafa Majid Al-Saedi <g00064937@aus.edu>
2019-02-10 14:39:00 +0400

    Initial version

---
+++ src/coe420la2/COE420LA2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420la2;
+
+/**
+ *
+ * @author g00064937
+ */
+public class COE420LA2 {
+
+    public static void addfeature(){
+        System.out.println ("Fubnction called");
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        addfeature();
+    }
+    
+}
